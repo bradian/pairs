dba320217ff8ebe5bb6e1d3a0105e47da6179b58
bradian <ian@ianbradford.uk>
2019-08-29 10:18:13 +0200

    Pairs exercise solution without generics - two objects in tester

--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,9 @@
 public class Tester {
     public static void main(String args[]){
 
-        Pair pair = new Pair ("one", "1");
-        System.out.println(pair);
+        Pair pair1 = new Pair ("one", "1");
+        Pair pair2 = new Pair ("two", "2");
+        System.out.println(pair1);
+        System.out.println(pair2);
     }
 }
